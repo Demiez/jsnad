455f31f0418b031c465de8c98e0ad6372a51e33f
Dmytro Petrenko <dmg.petrenko@gmail.com>
2022-01-19 09:38:07 +0200

    07 - 2 modules in 1 file

--- 07_nodes_module_system/README.md
+++ 07_nodes_module_system/README.md
@@ -1,3 +1,11 @@
 # Node's module system
 
 ### CLI commands:
+
+- `node -e "require('./index.js')"` - befaves the same like simple node call
+
+### Main module:
+
+When a file is the entry point of a program, it's the main module. We can detect whether a particular file is the main module in two ways.<br>
+
+We can check if `module.parent` is `null` or we can check if `require.main` is the module object.
--- 07_nodes_module_system/my-package/index.js
+++ 07_nodes_module_system/my-package/index.js
@@ -1,6 +1,18 @@
 'use strict';
-const pino = require('pino');
 const { upper } = require('./format');
-const logger = pino();
-logger.info(upper('my-package started'));
-process.stdin.resume();
+
+// we check if file is the main module (both checks work)
+if (require.main === module && module.parent === null) {
+  const pino = require('pino');
+  const logger = pino();
+  logger.info(upper('my-package started'));
+  process.stdin.resume();
+} else {
+  const reverseAndUpper = (str) => {
+    return upper(str).split('').reverse().join('');
+  };
+  module.exports = reverseAndUpper;
+}
+
+// index.js has two operational modules
+// node -p "require('./index.js')('hello')" - If it is loaded as a module, it will export a function that reverses and upper-cases a string

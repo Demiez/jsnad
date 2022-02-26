a04ce38b8c77bb84a93f8df8f76260e5eae4bf23
Dmytro Petrenko <dmg.petrenko@gmail.com>
2022-02-26 10:40:02 +0200

    13 - lab2 finished

--- 13_interacting_with_file_system/labs-2/index.js
+++ 13_interacting_with_file_system/labs-2/index.js
@@ -1,59 +1,70 @@
-'use strict'
-const assert = require('assert')
-const { join } = require('path')
-const fs = require('fs')
-const { promisify } = require('util')
-const timeout = promisify(setTimeout)
-const project = join(__dirname, 'project')
-try { fs.rmdirSync(project, {recursive: true}) } catch (err) {
-  console.error(err)
+'use strict';
+const assert = require('assert');
+const { join } = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
+
+const timeout = promisify(setTimeout);
+const project = join(__dirname, 'project');
+
+try {
+  fs.rmdirSync(project, { recursive: true });
+} catch (err) {
+  console.error(err);
 }
-fs.mkdirSync(project)
 
-let answer = ''
+fs.mkdirSync(project);
 
-async function writer () {
-  const { open, chmod, mkdir } = fs.promises
-  const pre = join(project, Math.random().toString(36).slice(2))
-  const handle = await open(pre, 'w')
-  await handle.close()
-  await timeout(500)
-  exercise(project)
-  const file = join(project, Math.random().toString(36).slice(2))
-  const dir = join(project, Math.random().toString(36).slice(2))
-  const add = await open(file, 'w')
-  await add.close()
-  await mkdir(dir)
-  await chmod(pre, 0o444)
-  await timeout(500)
+let answer = '';
+
+async function writer() {
+  const { open, chmod, mkdir } = fs.promises;
+
+  const pre = join(project, Math.random().toString(36).slice(2));
+
+  const handle = await open(pre, 'w');
+
+  await handle.close();
+  await timeout(500);
+
+  exercise(project);
+
+  const file = join(project, Math.random().toString(36).slice(2));
+  const dir = join(project, Math.random().toString(36).slice(2));
+  const add = await open(file, 'w');
+
+  await add.close();
+  await mkdir(dir);
+  await chmod(pre, 0o444);
+  await timeout(500);
+
   assert.strictEqual(
-    answer, 
-    file, 
+    answer,
+    file,
     'answer should be the file (not folder) which was added'
-  )
-  console.log('passed!')
-  process.exit()
+  );
+  console.log('passed!');
+  process.exit();
 }
 
 writer().catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+  console.error(err);
+  process.exit(1);
+});
 
+function exercise(project) {
+  const files = new Set(fs.readdirSync(project));
 
-function exercise (project) {
-  const files = new Set(fs.readdirSync(project))
   fs.watch(project, (evt, filename) => {
-    try { 
-      const filepath = join(project, filename)
-      const stat = fs.statSync(filepath)
+    try {
+      const filepath = join(project, filename);
+      const stat = fs.statSync(filepath);
 
+      if (stat.ctimeMs === stat.mtimeMs && !stat.isDirectory()) {
+        answer = filepath;
+      }
       // TODO - only set the answer variable if the filepath
       // is both newly created AND does not point to a directory
-
-      answer = filepath
-    } catch (err) {
-
-    } 
-  })
+    } catch (err) {}
+  });
 }

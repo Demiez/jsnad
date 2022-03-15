171dbe5e822ac4404095b05e1c591acfee825411
Dmytro Petrenko <dmg.petrenko@gmail.com>
2022-03-15 17:40:52 +0200

    16 - jest framework

---
+++ 16_writing_unit_tests/test-env-jest/req.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = (url, cb) => {
+  setTimeout(() => {
+    if (url === 'ht‌tp://error.com') cb(Error('network error'));
+    else cb(null, Buffer.from('some data'));
+  }, 300);
+};
